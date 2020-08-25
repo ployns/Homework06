3df2fc5f8d006cfdc9c88f18fb0ea78a4ee3f616
P <P@MSI>
2020-08-25 13:31:45 +0700

    แก้ไข

--- Homework06/Source.cpp
+++ Homework06/Source.cpp
@@ -4,19 +4,19 @@ int main()
 	int a;
 	printf("Hight : ");
 	scanf_s("%d", &a);
-	if (a >= 0) {
-		if (a >= 100 && a < 140)
+	if (a >= 60) {
+		if (a >= 60 && a < 140)
 			printf("very small\n");
 		else if (a >= 140 && a < 155)
 			printf("small\n");
 		else if (a >= 155 && a < 165)
 			printf("normal\n");
-		else
+		else if (a >= 165 && a < 250)
 			printf("tall\n");
 	}
 	else {
 		printf("error\n");
-		printf("Try again");
+		printf("Try again\n");
 	}
 	printf("Thank you");
 }
